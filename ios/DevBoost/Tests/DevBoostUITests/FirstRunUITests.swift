ef8b840948923ddcd6a686daa42ef9ff05fd1356
kubilaycaglayan <kblycaglayan@gmail.com>
2026-09-06 21:18:43 +0300

    test(ios): cover home tile background bleed

--- ios/DevBoost/Tests/DevBoostUITests/FirstRunUITests.swift
+++ ios/DevBoost/Tests/DevBoostUITests/FirstRunUITests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import UIKit
 
 @MainActor
 final class FirstRunUITests: XCTestCase {
@@ -16,6 +17,26 @@ final class FirstRunUITests: XCTestCase {
         XCTAssertTrue(app.tabBars.buttons["Docker"].exists)
         XCTAssertTrue(app.tabBars.buttons["Transfer"].exists)
         XCTAssertTrue(app.tabBars.buttons["Usage"].exists)
+    }
+
+    func testHomeTilesDoNotTintTheSurroundingMargin() {
+        let app = launchFresh()
+        let tileIDs = ["home-feature-hosts", "home-feature-docker", "home-feature-usage"]
+        let tiles = tileIDs.map { app.buttons[$0] }
+
+        for tile in tiles {
+            XCTAssertTrue(tile.waitForExistence(timeout: 5))
+            XCTAssertGreaterThan(tile.frame.minX, 10)
+        }
+
+        let screenshot = XCUIScreen.main.screenshot()
+        for tile in tiles {
+            let samplePoint = CGPoint(x: tile.frame.minX - 8, y: tile.frame.midY)
+            let color = screenshot.pixelColor(at: samplePoint)
+            XCTAssertLessThan(color.red, 0.10, "Tile decoration leaked into the left margin")
+            XCTAssertLessThan(color.green, 0.10, "Tile decoration leaked into the left margin")
+            XCTAssertLessThan(color.blue, 0.12, "Tile decoration leaked into the left margin")
+        }
     }
 
     func testFirstRunEmptyStatesExplainWhatToDoNext() {
@@ -46,5 +67,34 @@ final class FirstRunUITests: XCTestCase {
         app.textFields["Username"].tap()
         app.textFields["Username"].typeText("ubuntu")
         XCTAssertTrue(save.isEnabled)
+    }
+}
+
+private extension XCUIScreenshot {
+    func pixelColor(at point: CGPoint) -> (red: CGFloat, green: CGFloat, blue: CGFloat) {
+        let image = image.cgImage!
+        let scaleX = CGFloat(image.width) / UIScreen.main.bounds.width
+        let scaleY = CGFloat(image.height) / UIScreen.main.bounds.height
+        let x = Int(point.x * scaleX)
+        let y = Int(point.y * scaleY)
+
+        var pixel = [UInt8](repeating: 0, count: 4)
+        let context = pixel.withUnsafeMutableBytes {
+            CGContext(
+                data: $0.baseAddress,
+                width: 1,
+                height: 1,
+                bitsPerComponent: 8,
+                bytesPerRow: 4,
+                space: CGColorSpaceCreateDeviceRGB(),
+                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+            )!
+        }
+        context.draw(image, in: CGRect(x: -x, y: -y, width: image.width, height: image.height))
+        return (
+            CGFloat(pixel[0]) / 255,
+            CGFloat(pixel[1]) / 255,
+            CGFloat(pixel[2]) / 255
+        )
     }
 }
